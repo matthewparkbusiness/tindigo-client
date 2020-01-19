29e60a9b6ba6d93d0fae669d57c3f12c0316a4ef
Matthew Park <42101816+matthewpark@users.noreply.github.com>
2020-01-18 22:41:49 -0800

    Updates

---
+++ precache-manifest.375f03c8edbd07c796d5b82ec27bef9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7bf830534a9bdf38f5b88426c5ca18",
+    "url": "/tindigo-client/index.html"
+  },
+  {
+    "revision": "0ef031bde8bfd5c1e18e",
+    "url": "/tindigo-client/static/css/main.17dfedfb.chunk.css"
+  },
+  {
+    "revision": "bcdf1e4b65d66c58f403",
+    "url": "/tindigo-client/static/js/2.117684d1.chunk.js"
+  },
+  {
+    "revision": "0ef031bde8bfd5c1e18e",
+    "url": "/tindigo-client/static/js/main.48b7556e.chunk.js"
+  },
+  {
+    "revision": "ef0b4a6d91cbd4ba54f5",
+    "url": "/tindigo-client/static/js/runtime~main.d3e972f4.js"
+  },
+  {
+    "revision": "543f1ecc5b457d6ee3733c5a9d429f53",
+    "url": "/tindigo-client/static/media/profile-pic-placeholder.543f1ecc.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tindigo-client/precache-manifest.375f03c8edbd07c796d5b82ec27bef9b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tindigo-client/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
